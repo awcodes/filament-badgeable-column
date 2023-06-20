bcd8a02a42957c0453e0b87d32a0b54749319ff1
thijmen <mail@thijmenkort.nl>
2023-06-20 09:23:51 +0200

    Change to match statement

--- resources/views/components/badge.blade.php
+++ resources/views/components/badge.blade.php
@@ -7,15 +7,17 @@
 <span
     @class([
         'filament-badgeable-badge px-2 inline-flex  text-xs font-medium',
-        'text-primary-700 bg-primary-500/10 dark:text-primary-500' => $color === 'primary',
-        'text-secondary-700 bg-secondary-500/10 dark:text-secondary-500' => $color === 'secondary',
-        'text-success-700 bg-success-500/10 dark:text-success-500' => $color === 'success',
-        'text-warning-700 bg-warning-500/10 dark:text-warning-500' => $color === 'warning',
-        'text-danger-700 bg-danger-500/10 dark:text-danger-500' => $color === 'danger',
-        'text-gray-700 bg-gray-500/10 dark:text-gray-500' => $color === 'default' || $color === 'gray',
-        'text-gray-700' => $invertTextColor() && ! $getTextColor(),
+        match($color) {
+            'primary' => 'text-primary-700 bg-primary-500/10 dark:text-primary-500',
+            'secondary' => 'text-secondary-700 bg-secondary-500/10 dark:text-secondary-500',
+            'success' => 'text-success-700 bg-success-500/10 dark:text-success-500',
+            'warning' => 'text-warning-700 bg-warning-500/10 dark:text-warning-500',
+            'danger' => 'text-danger-700 bg-danger-500/10 dark:text-danger-500',
+            'default', 'gray' => 'text-gray-700 bg-gray-500/10 dark:text-gray-500',
+        },
+        'text-gray-700' => $invertTextColor() && $getTextColor(),
         'rounded py-1' => ! $isPill,
-        'rounded-full py-0.5' => $isPill,
+        'rounded-full py-0.5' => $isPill
     ])
     {!! $hasHexColor() ? "style=\"background-color:" . $color . "; color:" . $getTextColor() . " !important;\"" : null !!}
 >{{ $getLabel() }}</span>
